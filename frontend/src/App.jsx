a5e99b7e6f816e349f2a4d11864a10377a6ac1b3
claurine <laurine.camuset@ynov.com>
2026-04-14 09:10:32 +0200

    feat/frontend(RegisterPage) : Initialized Register page visual and fixed Login page issue and added switching button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginView from "../Views/LoginView";
+import RegisterView from "../Views/RegisterView";
 
 function App() {
-  return <LoginView />;
+  return (
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/register" element={<RegisterView />} />
+      </Routes>
+  );
 }
 
 export default App;
